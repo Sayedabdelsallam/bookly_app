fc66dcb8534b8ecebfde4bcf2f29675abf1e6cd2
Sayedabdelsallam <abdelslamsayed288@gmail.com>
2025-05-15 03:33:34 +0300

    add animation to splash view body. finish splash ui.

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:bookly_app/core/constans/app_assets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+    )..repeat(reverse: true); // Loops the animation
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.2).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        ScaleTransition(
+          scale: _scaleAnimation,
+          child: Image.asset(AppAssets.logo),
+        ),
+        SizedBox(
+          height: 8.h,
+        ),
+        Text(
+          'Read Free Books',
+          style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                color: Colors.white,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // Add this
 import 'package:get/get.dart';
 
+import 'core/constans/color_pattele.dart';
 import 'features/splash/splash_view.dart';
-
 
 void main() {
   runApp(const BooklyApp());
@@ -13,10 +14,20 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-
-      home: SplashView(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690), // Default design size (adjust based on your design)
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData().copyWith(
+            scaffoldBackgroundColor: AppColors.primaryColor,
+          ),
+          home: child,
+        );
+      },
+      child: const SplashView(), // Your initial screen
     );
   }
 }
-
