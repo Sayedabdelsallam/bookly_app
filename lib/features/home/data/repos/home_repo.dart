977dabec787319cae8fba13e784f61decbc08e04
Sayedabdelsallam <abdelslamsayed288@gmail.com>
2025-05-25 06:53:58 +0300

    create api service. create failures file. add dartz.

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,10 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+
 abstract class HomeRepo
 {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either< Failure, List<BookModel>>> fetchNewestBooks();
+  Future<Either< Failure, List<BookModel>>> fetchFeaturedBooks();
 }
