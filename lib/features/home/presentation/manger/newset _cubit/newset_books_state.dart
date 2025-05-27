e30a0edada63d2118af72078129e3ff4fefe2966
Sayedabdelsallam <abdelslamsayed288@gmail.com>
2025-05-27 05:12:15 +0300

    create Newest Books Cubit.

---
+++ lib/features/home/presentation/manger/newset _cubit/newset_books_state.dart
@@ -0,0 +1,21 @@
+part of 'newset_books_cubit.dart';
+
+abstract class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class NewsetBooksInitial extends NewsetBooksState {}
+final class NewsetBooksLoading extends NewsetBooksState {}
+final class NewsetBooksFailure extends NewsetBooksState
+{
+  final String errMessage;
+  const NewsetBooksFailure(this.errMessage);
+}
+final class NewsetBooksSuccess extends NewsetBooksState
+{
+  final List<BookModel> books;
+  const NewsetBooksSuccess(this.books);
+}
